1f73d7dee00cb8232499e1f5bc9f980d8a72f97d
thinh909thinh <51990581+thinh909thinh@users.noreply.github.com>
2022-03-16 12:02:52 +0700

    Add files via upload

---
+++ vd61.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+ int main(){
+ int a;
+ float b = 24.67892345;
+ char ch = 'A';
+ printf("Integer data = %d", a);
+ printf("Float Data = %f",b);
+ printf("Character = %c",ch);
+ printf("\nThis prints the string");
+ printf("%s","\nThis also prints a string");
+}
